40b686f4ba58194c830509824632307556113a5a
Kadin Sayani <kadinsayani8@gmail.com>
2022-02-12 12:04:06 -0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Wellness Tracker",
+    subtitle: "Java, JavaFX",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Konsume",
+    subtitle: "MERN",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "",
+    link: "",
+  },
+];
+
+export const skills = ["JavaScript"];
